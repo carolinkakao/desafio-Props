d8f130f1241dd737fab8644e914ed800ccad5e73
Carolina Gonzalez <c.gonzalezcontrera@gmail.com>
2021-04-10 18:08:24 -0400

    agregando proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="./assets/logo.png" />   
+    <h1>Planificación de tareas</h1>
+    <section class="mt-3">
+    <input placeholder="Escribe tu tarea" type="text" v-model="tarea" @keyup.enter="agregarTarea()">    <!-- v-model: hace la conexión directa entre input y js, texto escrito en input es detectado por vue//@keyup.enter= para agregar tareas con el enter-->
+    <button @click="agregarTarea()" type="button" class="btn btn-danger ms-4">Añade tu Tarea</button>  <!--evento click  -->
+    </section>
+    <Hijo :tareaHijo="misTareas" @quitarTarea="eliminarTarea" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Hijo from "./components/Hijo.vue";
 
 export default {
   name: "App",
+ data(){
+   return{
+     misTareas:[
+       /*
+       {tarea:"", cantidad:""},
+       {tarea:"",cantidad:""},
+       {tarea:"",cantidad:""}
+       */
+     ],
+     tarea:"",
+     cantidad:""
+   }
+ },
   components: {
-    HelloWorld,
+    Hijo,
   },
+  methods:{
+    agregarTarea(){
+    if(this.tarea !== ""){ 
+          this.misTareas.push({
+          tarea: this.tarea
+          })
+      this.tarea = "" }else{ /* this.tarea="" --> para que se limpie el imput al momento de escribir*/
+        alert("Debes escribir tu tarea"); 
+    }
+    },
+    eliminarTarea(eliminar){
+      this.misTareas.splice(eliminar,1); /*con slice puedo extraer la parte que quiera de un array*/
+    }
+  }
 };
 </script>
 
---
+++ src/components/Hijo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mt-5">
+    <ul>
+    <li v-for="(resultadoTareas,index) in tareaHijo" :key="index">Tarea {{index+1}}: {{resultadoTareas.tarea}}
+      <input type="number" placeholder="Nº repetición tarea" v-model.number="resultadoTareas.cantidad"/>
+      <button class="btn-info"  @click="resultadoTareas.cantidad ++">+</button>
+    <button class="btn-danger" @click.prevent="quitarTarea(index)">Eliminar</button>
+
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hijo",
+  props: {
+    tareaHijo:{
+      type:Array,
+      default: () => []
+    }
+  },
+  methods:{
+    quitarTarea(eliminar){ /*a traves de metodos el hijo se comunica o envia datos al padre*/
+      this.$emit('quitarTarea',eliminar)/*emit=emite un metodo*/
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
